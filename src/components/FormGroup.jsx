bcb384e13be98d0d04ba554aedcd204669a865c8
Sahar Mirkamrani <shrmrkmrn@gmail.com>
2021-05-08 17:11:59 +0430

    First page done

--- src/components/FormGroup.jsx
+++ src/components/FormGroup.jsx
@@ -21,6 +21,28 @@ const initialValues = {
     house: '',
     dailyRent: '',
   },
+  otherServices: '',
+  workHourFrom: '',
+  workHourTo: '',
+  responseHourFrom: '',
+  responseHourTo: '',
+  consultantsInPerson: '',
+  consultantsOnline: '',
+  dailyInPersonClients: '',
+  dailyOnlineClients: '',
+  ownerCount: '',
+  buyerCount: '',
+  renterCount: '',
+  renteeCount: '',
+  otherClients: '',
+  adAdpproach: '',
+  workWithAdWebsites: '',
+  doTheyUsePortals: '',
+  whatSitesTheyUse: '',
+  toAttractsClients: '',
+  wantToPayMoreForAttractingClients: '',
+  budgetForAd: '',
+  bestWayToAd: '',
 };
 
 const Form = () => {
@@ -258,6 +280,331 @@ const Form = () => {
               onChange={handleInputChange}
               name="dailyRent"
               id="dailyRent"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.singleInput}>
+            <label className={styles.label}>
+              {' '}
+              * خدمات دیگر (توضیح دهید) :{' '}
+            </label>
+            <input
+              type="text"
+              className={styles.longInput}
+              variant="filled"
+              value={values.otherServices}
+              onChange={handleInputChange}
+              name="otherServices"
+            />
+          </div>
+        </div>
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}> * ساعت کاری از </label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.workHourFrom}
+              onChange={handleInputChange}
+              name="workHourFrom"
+            />
+            <label className={styles.label}>{'تا'}</label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.workHourTo}
+              onChange={handleInputChange}
+              name="workHourTo"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}> * ساعت پاسخگویی از </label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.responseHourFrom}
+              onChange={handleInputChange}
+              name="responseHourFrom"
+            />
+            <label className={styles.label}>{'تا'}</label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.responseHourTo}
+              onChange={handleInputChange}
+              name="responseHourTo"
+            />
+          </div>
+        </div>
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}> * تعداد مشاور حاضر در محل </label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.consultantsInPerson}
+              onChange={handleInputChange}
+              name="consultantsInPerson"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}> * تعداد مشاور غیر حضوری </label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.consultantsOnline}
+              onChange={handleInputChange}
+              name="consultantsOnline"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}> تعداد مراجعین حضوری در روز </label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.clientsInPerson}
+              onChange={handleInputChange}
+              name="clientsInPerson"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}>
+              {' '}
+              تعداد مراجعین غیر حضوری در روز{' '}
+            </label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.dailyOnlineClients}
+              onChange={handleInputChange}
+              name="dailyOnlineClients"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}>
+              {' '}
+              تعداد مراجعین مربوط به خرید و فروش روزانه :
+            </label>
+
+            <label className={styles.label}> مالک</label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.ownerCount}
+              onChange={handleInputChange}
+              name="ownerCount"
+            />
+            <label className={styles.label}> خریدار</label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.buyerCount}
+              onChange={handleInputChange}
+              name="buyerCount"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}>
+              {' '}
+              تعداد مراجعین مربوط به رهن و اجاره روزانه :
+            </label>
+
+            <label className={styles.label}> موجر</label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.renterCount}
+              onChange={handleInputChange}
+              name="renterCount"
+            />
+            <label className={styles.label}> مستاجر</label>
+            <input
+              type="number"
+              className={styles.shortInput}
+              variant="filled"
+              value={values.renteeCount}
+              onChange={handleInputChange}
+              name="renteeCount"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}> * تعداد مراجعین موارد دیگر </label>
+            <input
+              type="text"
+              className={styles.longInput}
+              variant="filled"
+              value={values.otherClients}
+              onChange={handleInputChange}
+              name="otherClients"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}>
+              {' '}
+              از چه طریقی اطلاع رسانی میکنید ؟{' '}
+            </label>
+            <input
+              type="text"
+              className={styles.input}
+              variant="filled"
+              value={values.adAdpproach}
+              onChange={handleInputChange}
+              name="adAdpproach"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}>
+              {' '}
+              آیا با سایت های تبلیغاتی کار میکنید ؟{' '}
+            </label>
+            <input
+              type="text"
+              className={styles.input}
+              variant="filled"
+              value={values.workWithOtherAdWebsites}
+              onChange={handleInputChange}
+              name="workWithOtherAdWebsites"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}>
+              {' '}
+              آیا از پرتال های معرفی مثل دیوار استفاده میکنید ؟{' '}
+            </label>
+            <input
+              type="text"
+              className={styles.input}
+              variant="filled"
+              value={values.doTheyUsePortals}
+              onChange={handleInputChange}
+              name="doTheyUsePortals"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}>
+              {' '}
+              از چه سایت هایی برای معرفی فایل ها استفاده میکنید ؟{' '}
+            </label>
+            <input
+              type="text"
+              className={styles.input}
+              variant="filled"
+              value={values.whatSitesTheyUse}
+              onChange={handleInputChange}
+              name="whatSitesTheyUse"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}>
+              {' '}
+              جهت جذب مشتری چه تبلیغاتی انجام میدهید ؟{' '}
+            </label>
+            <input
+              type="text"
+              className={styles.longInput}
+              variant="filled"
+              value={values.toAttractsClients}
+              onChange={handleInputChange}
+              name="toAttractsClients"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}>
+              {' '}
+              آیا تمایل دارید برای جذب مشتری بیشتر هزینه کنید ؟{' '}
+            </label>
+            <input
+              type="text"
+              className={styles.input}
+              variant="filled"
+              value={values.wantToPayMoreForAttractingClients}
+              onChange={handleInputChange}
+              name="wantToPayMoreForAttractingClients"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}>
+              {' '}
+              چقدر بودجه تبلیغاتی دارید ؟{' '}
+            </label>
+            <input
+              type="number"
+              className={styles.input}
+              variant="filled"
+              value={values.budgetForAd}
+              onChange={handleInputChange}
+              name="budgetForAd"
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div>
+            <label className={styles.label}>
+              بهترین نوع تبلیغات برای کار شما چیست ؟
+            </label>
+            <input
+              type="text"
+              className={styles.input}
+              variant="filled"
+              value={values.bestWayToAd}
+              onChange={handleInputChange}
+              name="bestWayToAd"
             />
           </div>
         </div>
